a97bfd067bd6d8fb8399790a727da98967601272
Ilya Syrmolot <syrmolotilya@gmail.com>
2025-02-18 19:28:30 +1000

    .

--- Binance API/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# BTC Price
-response = requests.get('https://api.binance.com/api/v3/ticker/price', params={'symbol': 'BTCUSDT'})
-print('Price:')
-print(response.json()['price'])
